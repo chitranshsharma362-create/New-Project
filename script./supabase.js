3972bad43eacb322a44586e46f565ed9df49617a
chitranshsharma362-create <chitranshsharma362@gmail.com>
2026-01-15 10:00:24 +0530

    Create supabase.js

---
+++ script./supabase.js
@@ -0,0 +1,10 @@
+const supabaseClient = window.supabase.creatClient(
+  "https://veeksbpngjfdwhbikxys.supabase.co",
+  "sb_publishable_Fd9uiZ_5THN8T3svE6mnDw_0uie3n_q"
+  );
+
+if (supabaseClient) {
+  console.log("Database connected:", supabaseClient);
+} else{
+  console.log("Database not connected");
+}
