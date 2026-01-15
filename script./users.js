3897e1418b3ddf908609dd86784aec8a88039779
chitranshsharma362-create <chitranshsharma362@gmail.com>
2026-01-15 11:20:08 +0530

    Create users.js

---
+++ script./users.js
@@ -0,0 +1,49 @@
+document.getElementById("form").addEventLiastner("submit" , async (e) => {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value.trim();
+  const email = document.getElementById("email").value.trim().tolowerCase();
+  const password = document.getElementById("password").value.trim();
+
+  if (!name || !email || !password){
+    alert("all fields are required");
+    return;
+  }
+
+  const {data: existingUser, error: checkError } =
+    await supabaseClient
+  .form("users")
+  .select("id")
+  .eq("email", email)
+  .maybeSingle();
+
+  if (checkError){
+    console.error(checkError);
+    alert("Error checking user");
+    return;
+  }
+
+  if (existingUser) {
+    alert("Email already registerd");
+    return;
+  }
+
+  const { error: insertError} =
+    await supabaseClient
+  .from("users");
+  .insert([
+  {
+    name: name;
+    email: email;
+    password: password;
+}
+])
+
+if(insertError){
+  console.error("registration failed");
+  return;
+}
+
+alert("registration successfully");
+e.target.reset();
+})
